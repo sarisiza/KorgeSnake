9b9a3773ec20fc9ff501d421d856ae076f9a8d29
sarisiza <sarita.iza@gmail.com>
2026-02-06 18:27:54 -0600

    Create Snake

--- src/snake/SnakePiece.kt
+++ src/snake/SnakePiece.kt
@@ -4,5 +4,6 @@ import korlibs.math.geom.*
 
 data class SnakePiece(
     val position: PointInt,
+    val orientation: BodyOrientation = BodyOrientation.HORIZONTAL_LEFT,
     val isHead: Boolean = false,
 )
